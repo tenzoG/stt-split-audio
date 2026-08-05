682aa1be64f4e4ef6061168f6fc45a432e2ec72a
Tenzin Choten <choten@Choten.local>
2026-08-05 17:51:55 +0530

    ab

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 
 # Run yt_download.py
 echo "Running docx_download.py..."
-python ../audio_download_and_split/yt_download.py --config ../json_config/cs_config.json
+python ../audio_download_and_split/yt_download.py --config ../json_config/ab_config.json
 if [ $? -ne 0 ]; then
   echo "Error running yt_download.py. Exiting..."
   exit 1
@@ -10,7 +10,7 @@ fi
 
 # Run run_inference.py
 echo "Running run_inference.py..."
-python ../inference_runner/run_inference.py --config ../json_config/cs_config.json
+python ../inference_runner/run_inference.py --config ../json_config/ab_config.json
 if [ $? -ne 0 ]; then
   echo "Error running run_inference.py. Exiting..."
   exit 1
@@ -18,7 +18,7 @@ fi
 
 # Run make_csv.py
 echo "Running make_csv.py..."
-python ../make_db_csv/make_csv.py --config ../json_config/cs_config.json
+python ../make_db_csv/make_csv.py --config ../json_config/ab_config.json
 if [ $? -ne 0 ]; then
   echo "Error running make_csv.py. Exiting..."
   exit 1
@@ -38,7 +38,6 @@ fi
 # python ../make_db_csv/download_doc.py --config ../json_config/ab_config_etext.json
 
 echo "All scripts ran successfully!"
-
 
 
 """
